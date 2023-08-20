d325257a626793db4e524e92a5c53ec6b1d5c3fc
Jadhav Ganesh <dev.ganesh.jadhav@gmail.com>
2023-08-20 21:45:25 +0530

    Version-1 schema version 1 , basic Avro schema producer and consumer using schema registery and custom deserilizer

---
+++ src/main/java/com/gj/kafka/deserializer/CustomAvroDeserilizer.java
@@ -0,0 +1,28 @@
+package com.gj.kafka.deserializer;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
+import org.apache.avro.Schema;
+import org.apache.kafka.common.errors.SerializationException;
+
+public class CustomAvroDeserilizer extends KafkaAvroDeserializer {
+
+    @Override
+    public Object deserialize(String s, byte[] bytes) {
+        try {
+            return deserialize(bytes);
+        } catch (SerializationException e) {
+            System.out.println("Error in deserialization :" + e.getMessage());
+            return null;
+        }
+    }
+
+    public Object deserialize(String s, byte[] bytes, Schema readerSchema) {
+        try {
+            return deserialize(bytes, readerSchema);
+        } catch (SerializationException e) {
+            System.out.println("Error in deserialization :" + e.getMessage());
+            return null;
+        }
+    }
+}
