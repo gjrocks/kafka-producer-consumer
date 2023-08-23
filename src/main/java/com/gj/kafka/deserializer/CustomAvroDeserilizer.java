526fcd6cc0d0d6ec49aa462c45d2435caa0962be
Jadhav Ganesh <dev.ganesh.jadhav@gmail.com>
2023-08-23 12:34:31 +0530

    Version-1 schema version 1 , basic Avro schema producer and consumer using schema registery and custom deserilizer
    4)	None compatibility : schema with topic, various versions, modified version.  With specifictype , you have to override the avro deserilizer and add few null checks in the consumer.

--- src/main/java/com/gj/kafka/deserializer/CustomAvroDeserilizer.java
+++ src/main/java/com/gj/kafka/deserializer/CustomAvroDeserilizer.java
@@ -1,5 +1,6 @@
 package com.gj.kafka.deserializer;
 
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 
 import org.apache.avro.Schema;
@@ -10,15 +11,21 @@ public class CustomAvroDeserilizer extends KafkaAvroDeserializer {
     @Override
     public Object deserialize(String s, byte[] bytes) {
         try {
+
             return deserialize(bytes);
+
         } catch (SerializationException e) {
             System.out.println("Error in deserialization :" + e.getMessage());
             return null;
         }
     }
 
+
+
     public Object deserialize(String s, byte[] bytes, Schema readerSchema) {
         try {
+
+            System.out.println("Schema type: " + readerSchema.getType());
             return deserialize(bytes, readerSchema);
         } catch (SerializationException e) {
             System.out.println("Error in deserialization :" + e.getMessage());
