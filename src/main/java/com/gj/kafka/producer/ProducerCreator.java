13a3e926becdd81699f9e894096a595aac5e0be1
Jadhav Ganesh <dev.ganesh.jadhav@gmail.com>
2023-12-07 14:20:41 +0530

    RecordNameStrategy -version2 schema modified, producer code works, generic consumer works.

--- src/main/java/com/gj/kafka/producer/ProducerCreator.java
+++ src/main/java/com/gj/kafka/producer/ProducerCreator.java
@@ -5,9 +5,11 @@ import java.util.Properties;
 import com.gj.kafka.model.City;
 import com.gj.kafka.serializer.CitySerializer;
 import com.gj.kafka.util.Util;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -16,11 +18,15 @@ import com.gj.kafka.constants.IKafkaConstants;
 public class ProducerCreator {
 
 	public static Producer<String, City> createProducer(String broker) {
+
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, IKafkaConstants.CLIENT_ID);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CitySerializer.class.getName());
+		props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+		props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+		props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin\";");
 		return new KafkaProducer<>(props);
 	}
 
